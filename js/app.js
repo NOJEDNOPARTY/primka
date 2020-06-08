e08d75f4f644d6652c12f721d16be64b73996388
NOJEDNOPARTY <bordyuh@gmail.com>
2020-06-08 22:10:49 +0300

    'add pc ver without popup

--- js/app.js
+++ js/app.js
@@ -20,7 +20,32 @@ var common = {
 
 	},
 	owl: function(){
-		
+		$('.services-slider').owlCarousel({
+			loop:true,
+			margin:0,
+			nav: true,
+			dots: false,
+			items: 1,
+			autoplay:true,
+			autoplayTimeout:4000,
+			autoplayHoverPause:true,
+			animateOut: 'fadeOut',
+			animateIn: 'fadeIn',
+			smartSpeed:450
+		})
+		$('.sertificates-slider').owlCarousel({
+			loop:true,
+			margin: 17,
+			nav: true,
+			dots: false,
+			items: 4,
+			autoplay:true,
+			autoplayTimeout:4000,
+			autoplayHoverPause:true,
+			animateOut: 'fadeOut',
+			animateIn: 'fadeIn',
+			smartSpeed:450
+		})
 	},
 };
 
